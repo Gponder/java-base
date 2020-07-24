cba46ec2743b06df8fedda1643ffb195eb381ba2
gaoyaqin <yaqin-gao@foxmail.com>
2020-07-24 14:59:43 +0800

    thread

--- src/com/gponder/thread/lock/CAS.java
+++ src/com/gponder/thread/lock/CAS.java
@@ -6,7 +6,7 @@ package com.gponder.thread.lock;
  * @create 2020/7/22 17:26
  *
  * 乐观锁
- * 乐观锁在Java中是通过使用无锁编程来实现，最常采用的是CAS算法，Java原子类中的递增操作就通过CAS自旋实现的
+ * 乐观锁在Java中是通过使用无锁编程来实现，最常采用的是CAS算法，Java原子类中的递增操作就通过CAS(compare and swap)自旋实现的
  * @see java.util.concurrent.ConcurrentHashMap 1.8 采用CAS实现
  */
 public class CAS {
