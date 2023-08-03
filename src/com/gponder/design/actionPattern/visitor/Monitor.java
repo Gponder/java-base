ef7a2c745c33e105f5b2822e57d81f02c6beafe2
Gponder <Gao123!@#>
2023-08-03 11:08:27 +0800

    访问者

---
+++ src/com/gponder/design/actionPattern/visitor/Monitor.java
@@ -0,0 +1,8 @@
+package com.gponder.design.actionPattern.visitor;
+
+public class Monitor implements ComputerPart {
+    @Override
+    public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartVisitor.visit(this);
+    }
+}
